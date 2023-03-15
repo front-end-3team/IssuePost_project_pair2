469347b5f379b162533cdbd6e925804cca4d4c4a
minsik0726 <kimminsik0726@gmail.com>
2023-03-16 00:02:40 +0900

    Add: rtk 사용을 위한 초기세팅

--- issue/src/pages/test.js
+++ issue/src/pages/test.js
@@ -5,10 +5,17 @@ function Test() {
   const [users, setUsers] = useState();
   const getData = async () => {
     try {
-      const response = await axios.get(process.env.REACT_APP_BACKEND_URL);
+      const response = await axios.get(
+        process.env.REACT_APP_BACKEND_URL + "?per_page=25&page=1"
+      );
+
+      const response2 = await axios.get(
+        process.env.REACT_APP_BACKEND_URL + "?per_page=25&page=2"
+      );
 
       setUsers(response.data);
       console.log(response.data);
+      console.log(response2.data);
     } catch {
       console.log("error");
     }
